2924669f858fd0f8dba4e2dda7b14dbfa0039196
groyloranger <dev+github@decisiveroll.com>
2018-04-17 22:32:58 -0400

    Create Player component

---
+++ src/components/Player.js
@@ -0,0 +1,18 @@
+// @flow
+import React from 'react';
+
+// import '../styles/Player.css';
+
+// type Props = {};
+
+// eslint-disable-next-line no-use-before-define
+// Player.defaultProps = {};
+
+export default function Player(/* props: Props */) {
+  return (
+    <div>
+      Player
+    </div>
+  );
+}
+
---
+++ src/fixtures/Player.fixture.js
@@ -0,0 +1,9 @@
+import Player from '../components/Player';
+
+export default [
+  {
+    component: Player,
+    name: 'default',
+  },
+];
+
