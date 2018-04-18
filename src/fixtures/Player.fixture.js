bf8f4a9f428c160efba86e96f236de857979a104
groyloranger <dev+github@decisiveroll.com>
2018-04-17 22:40:31 -0400

    Create fixtures

--- src/fixtures/Player.fixture.js
+++ src/fixtures/Player.fixture.js
@@ -3,7 +3,36 @@ import Player from '../components/Player';
 export default [
   {
     component: Player,
+    name: 'active',
+    props: {
+      active: true,
+      bet: 1,
+      name: 'Hero',
+      stack: 199,
+    },
+  },
+  {
+    component: Player,
     name: 'default',
+  },
+  {
+    component: Player,
+    name: 'folded',
+    props: {
+      bet: 1,
+      folded: true,
+      name: 'Hero',
+      stack: 199,
+    },
+  },
+  {
+    component: Player,
+    name: 'inactive',
+    props: {
+      bet: 1,
+      name: 'Hero',
+      stack: 199,
+    },
   },
 ];
 
