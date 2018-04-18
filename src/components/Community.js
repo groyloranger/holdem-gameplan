4453760f37451f8a5930b8d24d59d9f1ead555d2
groyloranger <dev+github@decisiveroll.com>
2018-04-18 11:17:14 -0400

    Render board and pot

--- src/components/Community.js
+++ src/components/Community.js
@@ -4,15 +4,33 @@ import CSSModules from 'react-css-modules';
 
 import styles from '../styles/Community.module.css';
 
-// type Props = {};
+type Props = {
+  board?: string[],
+  pot?: number,
+};
 
 // eslint-disable-next-line no-use-before-define
-// Community.defaultProps = {};
+Community.defaultProps = {
+  board: [],
+  pot: 0,
+};
 
-function Community(/* props: Props */) {
+function Community(props: Props) {
+  function boardString() {
+    const b = ((props.board) ? props.board.slice() : []);
+    let str = '';
+    for (let len = 5; len > 0; len -= 1) {
+      const card = ((b.length === len) ? b.pop() : '__');
+      // build the string from right to left
+      str = ' '.concat(card).concat(str);
+    }
+    return str.trim();
+  }
+
   return (
     <div styleName="wrapper">
-      Community
+      <center styleName="board">{boardString()}</center>
+      <center styleName="pot">{props.pot}</center>
     </div>
   );
 }
--- src/fixtures/Community.fixture.js
+++ src/fixtures/Community.fixture.js
@@ -5,5 +5,36 @@ export default [
     component: Community,
     name: 'default',
   },
+  {
+    component: Community,
+    name: 'preflop',
+    props: {
+      pot: 2,
+    },
+  },
+  {
+    component: Community,
+    name: 'flop',
+    props: {
+      board: ['As', '5h', '8d'],
+      pot: 2,
+    },
+  },
+  {
+    component: Community,
+    name: 'turn',
+    props: {
+      board: ['As', '5h', '8d', 'Jc'],
+      pot: 2,
+    },
+  },
+  {
+    component: Community,
+    name: 'river',
+    props: {
+      board: ['As', '5h', '8d', 'Jc', '4s'],
+      pot: 2,
+    },
+  },
 ];
 
